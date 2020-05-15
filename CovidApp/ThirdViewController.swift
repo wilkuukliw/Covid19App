71860360865589984a74f7bc1b667bbc4855814b
plamistef <plamstef@icloud.com>
2020-05-15 09:59:21 +0200

    restart

--- CovidApp/ThirdViewController.swift
+++ CovidApp/ThirdViewController.swift
@@ -9,40 +9,18 @@
 import UIKit
 import Foundation
 
-class ThirdViewController: UIViewController, UITableViewDataSource {
+class ThirdViewController: UIViewController {
     
     
     
    let sections = ["Symptoms"]
-    let sympt = ["Headache", "Fever", "Cold"]
+   let sympt = ["Headache", "Fever", "Cold"]
+ 
+    
+    
 
     override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    // MARK: UITableViewDataSource
-
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-      return sections[section]
-    }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-      return sections.count
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        sympt.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      // Create an object of the dynamic cell "PlainCell"
-      let cell = tableView.dequeueReusableCell(withIdentifier: "PlainCell", for: indexPath)
-      // Depending on the section, fill the textLabel with the relevant text
-      cell.textLabel?.text = sympt[indexPath.row]
-        
-      // Return the configured cell
-      return cell
     }
 
     
